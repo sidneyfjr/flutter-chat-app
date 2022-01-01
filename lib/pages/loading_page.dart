3c664d6f06c6d7bf9ea8f3adf87eb4cdc8364559
sidney.silva <sidney.silva@sepog.fortaleza.ce.gov.br>
2022-01-01 18:11:19 -0300

    Fin de la Sección 10

---
+++ lib/pages/loading_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:chat/services/socket_service.dart';
+import 'package:chat/services/auth_service.dart';
+import 'package:chat/pages/login_page.dart';
+import 'package:chat/pages/usuarios_page.dart';
+
+class LoadingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder(
+        future: checkLoginState(context),
+        builder: (context, snapshot) {
+          return Center(
+            child: Text('Espere...'),
+          );
+        },
+      ),
+    );
+  }
+
+  Future checkLoginState(BuildContext context) async {
+
+    print('checkLoginState');
+
+    final authService = Provider.of<AuthService>(context, listen: false);
+    final socketService = Provider.of<SocketService>(context, listen: false);
+
+    print('checkLoginState2');
+
+    final autenticado = await authService.isLoggedIn();
+    
+    print(autenticado);
+
+    if (autenticado) {
+      socketService.connect();
+      Navigator.pushReplacement(
+          context,
+          PageRouteBuilder(
+              pageBuilder: (_, __, ___) => UsuariosPage(),
+              transitionDuration: Duration(milliseconds: 0)));
+    } else {
+      Navigator.pushReplacement(
+          context,
+          PageRouteBuilder(
+              pageBuilder: (_, __, ___) => LoginPage(),
+              transitionDuration: Duration(milliseconds: 0)));
+    }
+  }
+}
